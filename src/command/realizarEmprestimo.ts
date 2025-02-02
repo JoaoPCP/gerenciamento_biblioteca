9f2f616d07bb452e1a4b3ef01d472007708827df
Joao Paixão <joaopcp23@gmail.com>
2025-02-02 14:56:07 -0300

    init: inicialização da implementação dos commands

---
+++ src/command/realizarEmprestimo.ts
@@ -0,0 +1,16 @@
+// class realizarEmprestimo implements Command{
+//     execute(arg: {livro: string, usuario: Usuario}) { // mudar de string para classe Livro
+//         const {livro, usuario} = arg;
+//         if(!usuario){
+//             return('Usuário não encontrado')
+//         }
+//         if (!livro){
+//             return('Livro não encontrado')
+//         }
+//         if (usuario.isDevedor()){
+//             return('O usuario não pode realizar empréstimos pois possui emprestimos atrasados')
+//         }
+//         // exemplar
+//         if (livro.)
+//     }
+// }
